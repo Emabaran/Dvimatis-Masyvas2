9f1e71b314e6c9dc22b56ae31d8f51d0ae32f91e
Zygimantass <zygimantelis@gmail.com>
2018-01-13 08:33:10 +0200

    ruined

--- Source.cpp
+++ Source.cpp
@@ -4,26 +4,7 @@
 #include <string>
 
 using namespace std;
-const int Cn = 100;
-const int Cm = 100;
-const string CDfv = "Duomenys.txt";
-const string CFrez = "Rezultatai.txt";
-
-void Skaitymas(int P[][Cn], int & n, int & m);
-double Mokinio_Vidurkis(int n, int eil, int A[][Cn]);
-void Visu_Mokiniu_Vidurkiai(int n, double V[], int A[][Cn]);
-double Testo_Vidurkis(int m, int eil, int A[][Cn]);
-void Visu_Testu_Vidurkiai(int m, double T[], int A[][Cn]);
-void Spausdinimas(int k, string antraste, double M[]);
-
-int main() 
 {
-	int n, m;
-	int A[Cn][Cm];
-	double V[Cn], T[Cn];
-
-	ofstream fr(CFrez);
-	fr.close();
 
 	Skaitymas( A,  n,   m) ;
 	Visu_Mokiniu_Vidurkiai(m, V, A);
@@ -50,27 +31,11 @@ double Mokinio_Vidurkis(int n, int eil, int A[][Cn])
 {
 	double suma = 0;
 	for (int i = 0; i < n; i++) 
-	{
-		suma += A[eil][i];
-	}
 	return suma / n;
-}
-
-double Testo_Vidurkis(int m, int eil, int A[][Cn])
-{
-	double suma = 0;
-	for (int i = 0; i < m; i++) 
-	{
-		suma += A[i][eil];
-	}
 	return suma / m;
 }
 
 void Visu_Mokiniu_Vidurkiai(int n, double V[], int A[][Cn]) 
-{
-	for (int i = 0; i < n; i++) 
-		V[i] = Mokinio_Vidurkis(n, i, A);
-}
 
 void Visu_Testu_Vidurkiai(int m, double T[], int A[][Cn]) 
 {
@@ -85,13 +50,3 @@ void Spausdinimas(int k, string antraste, double M[])
 	fr << antraste << endl;
 	fr << "---------------------" << endl;
 	fr << "Eil nr.      Vidurkis" << endl;
-	fr << "-------------------" << endl;
-
-	for (int i = 0; i < k; i++) 
-	{
-		fr << setw(4) << i+1 << setw(14) << M[i] << endl;
-	}
-	fr << "-------------------" << endl << endl;
-
-	fr.close();
-}
